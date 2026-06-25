2014dff3aa1a462d15b46099514b7a55bae31d3f
yoav <yoavaa@gmail.com>
2026-06-25 18:01:32 +0300

    updated products page

--- src/components/site-header/site-header.ts
+++ src/components/site-header/site-header.ts
@@ -1,4 +1,25 @@
 import { makeJayStackComponent, phaseOutput } from '@jay-framework/fullstack-component';
+import { createSignal, createEffect } from '@jay-framework/component';
 
 export const SiteHeader = makeJayStackComponent()
-  .withSlowlyRender(async () => phaseOutput({}, {}));
+  .withFastRender(async () => {
+    return phaseOutput({ headerHidden: false }, {});
+  })
+  .withInteractive(function SiteHeader() {
+    const [isHidden, setIsHidden] = createSignal(false);
+
+    createEffect(() => {
+      const handler = () => {
+        const threshold = window.innerHeight * 0.5;
+        setIsHidden(window.scrollY > threshold);
+      };
+      window.addEventListener('scroll', handler, { passive: true });
+      return () => window.removeEventListener('scroll', handler);
+    });
+
+    return {
+      render: () => ({
+        headerHidden: isHidden(),
+      }),
+    };
+  });
